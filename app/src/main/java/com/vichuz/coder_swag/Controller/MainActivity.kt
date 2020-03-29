383178aec81d4c8cabd2ad31dafd929f2e253038
hari <krishnadude98@gmail.com>
2020-03-29 13:06:08 +0530

    prepare for recycler view adapter

--- app/src/main/java/com/vichuz/coder_swag/Controller/MainActivity.kt
+++ app/src/main/java/com/vichuz/coder_swag/Controller/MainActivity.kt
@@ -2,7 +2,9 @@ package com.vichuz.coder_swag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.vichuz.coder_swag.Adapters.CategoryAdapter
 import com.vichuz.coder_swag.Model.Category
 import com.vichuz.coder_swag.R
@@ -10,12 +12,12 @@ import com.vichuz.coder_swag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var adapter:CategoryAdapter
+    lateinit var adapter: CategoryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        adapter= CategoryAdapter(this,DataService.categories)
-        categoryListView.adapter=adapter
+        adapter = CategoryAdapter(this, DataService.categories)
+        categoryListView.adapter = adapter
 
     }
 }
